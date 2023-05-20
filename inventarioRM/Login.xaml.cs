2ad17b6eb0bb55a3a60233c6514727b51a9cc3d6
Jona2808 <jonathan28quina@gmail.com>
2023-05-19 21:54:01 -0500

    ultimo

--- inventarioRM/Login.xaml.cs
+++ inventarioRM/Login.xaml.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
 
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
@@ -12,9 +15,20 @@ namespace inventarioRM
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class Login : ContentPage
     {
+        private const string Url = "http://192.168.0.3/rmInventario/post.php";
+        private readonly HttpClient cliente = new HttpClient();
+        private ObservableCollection<inventarioRM.WS.Datos> post;
         public Login()
         {
             InitializeComponent();
+        }
+
+        private async void btnInicio_Clicked(object sender, EventArgs e)
+        {
+            Login log = new Login
+            {
+                Usuario = txt
+            };
         }
     }
 }
